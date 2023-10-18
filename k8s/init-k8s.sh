27140d4d534ff5bee853ecb2afbfca173f822525
Jakub Wasniewski <jakub.wasniewski@dynatrace.com>
2023-10-18 16:32:12 +0200

    Delete streama, add init-prometheus

--- k8s/init-k8s.sh
+++ k8s/init-k8s.sh
@@ -4,10 +4,14 @@
 ./clean-k8s.sh 2>/dev/null
 
 # checking if the streaming server image exists, if not build it
-if [[ "$(docker image inspect streaming-server 2> /dev/null)" == "[]" ]]; then
+if [[ "$(docker image inspect streaming-server:0.1.0 2> /dev/null)" == "[]" ]]; then
   echo "Image not present locally, building..."
-  (cd ../streaming-server ; docker build -t streaming-server .)
+  eval $(minikube docker-env)
+  (cd ../streaming-server ; docker build -t streaming-server:0.1.0 .)
 fi
 
 kubectl create -f ./streaming-server-deployment.yaml
 kubectl create -f ./streaming-server-service.yaml
+
+minikube addons enable ingress
+minikube tunnel
